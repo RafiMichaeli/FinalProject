b889815614972833b1130563731846675c0ce039
RafiMichaeli <rafi.michaeli@gmail.com>
2018-04-17 10:32:20 +0300

    routing and logout are working

--- app/shared/navbar/navbarCtrl.js
+++ app/shared/navbar/navbarCtrl.js
@@ -1,5 +1,9 @@
-mainHoaApp.controller("navbarCtrl", function ($scope, $http, $location, activeUserService){
-    
+mainHoaApp.controller("navbarCtrl", function ($scope, $http, $location, activeUserService) {
+
     $scope.currentUser = activeUserService.getUser();
+
+    $scope.logout = function () {
+        activeUserService.logout();
+    }
 
 })
