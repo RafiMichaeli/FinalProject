17fa360ce97aece03b87803a5a8213dbe4b95230
RafiMichaeli <rafi.michaeli@gmail.com>
2018-04-23 17:45:46 +0300

    wow, filter still not working but added google chrt (from outside in seperate js file)

--- app/components/voting/votingCtrl.js
+++ app/components/voting/votingCtrl.js
@@ -13,11 +13,12 @@ mainHoaApp.controller("votingCtrl", function ($scope, $http, $location, activeUs
 
     $scope.closedVotings = function () {
         for (var f = 0; f < $scope.votings.length;) {
-            var today = new Date();
-            if (($scope.votings[f].votingCloseDate) < today) {
-                
-            } return;
-        } f++;
+            if (($scope.votings[f].votingCloseDate) < new Date()) {
+                return;
+            } else {
+                f++;
+            }
+        }
     };
 
     $scope.openVotings = function () {
